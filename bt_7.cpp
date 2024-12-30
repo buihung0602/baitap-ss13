54f2c67c2accb0c52b6eef08d6475e86a3eda410
buihung0602 <n24dtcn035@student.ptithcm.edu.vn>
2024-12-30 23:43:37 +0700

    Add files via upload

--- bt_7.cpp
+++ bt_7.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-void intArray(int matrix[4][3], int row, int col);
+void intArray(int matrix[100][100], int row, int col);
 
 int main() {
-    int col = 3, row = 4;
-    int arr[4][3];
+    int row, col;
+
+    printf("nhap vao so hang: ");
+    scanf("%d", &row);
+    printf("nhap vao so cot: ");
+    scanf("%d", &col);
+
+    int arr[100][100];
 
     intArray(arr, row, col);
 
@@ -19,7 +25,7 @@ int main() {
     return 0;
 }
 
-void intArray(int matrix[4][3], int row, int col) {
+void intArray(int matrix[100][100], int row, int col) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             printf("matrix[%d][%d] = ", i, j);
